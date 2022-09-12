e8c30806d3ad5958629f150e79c84a6a87990204
nebiyat <nebiyats@gmail.com>
2022-09-11 19:27:42 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints possible combination of two two-digit numbers,
- * ranging from 0-99, separated by a comma followed by a space. 
+ * ranging from 0 - 99, separated by a comma followed by a space. 
  *
  * Return: always 0.
  */
@@ -18,18 +18,13 @@ int main(void)
 			puthcar((num1 % 10) + '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			
+			putchar((num2 % 10) + '0');	
 			if (num1 == 98 && num2 == 99)
 				continue;
-			
 			putchar(',');
 			putcahr(' ');
 		}
 	}
-	
 	putchar('\n');
-	
 	return (0);
 }
-
