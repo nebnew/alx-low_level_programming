38da1ac6aaeae7b8dbe42a4ef057fa24a4d30a49
nebiyat <nebiyats@gmail.com>
2022-09-13 20:14:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ *
+ * Desc; Header file containing declaration for all fuction
+ */
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,4 +18,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_time_table(int n);
+
 #endif
