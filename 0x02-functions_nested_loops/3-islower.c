f66ec0195384abaaf35991e48360f7ada7ec74e4
nebiyat <nebiyats@gmail.com>
2022-09-13 21:45:09 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for a lowercase character
+ * @c: letter being tested
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c<= 122)
+		Return (1);
+	else
+		Return (0);
+}
+ 
