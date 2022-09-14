3dbd5af693b8e279288bcb7679f75cc8efdb2494
nebiyat <nebiyats@gmail.com>
2022-09-14 12:13:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main -- print _putchar
  * Return: always 0
@@ -13,6 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');`
+	_putchar('\n');
+
 	return (0);
 }
