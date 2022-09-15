6f52e704338089452743b8ef776dd7e695f8fa27
nebiyat <nebiyats@gmail.com>
2022-09-14 17:12:06 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,12 +10,12 @@ int main(void)
 	int sum = 0;
 	
 	for (n = 0; n < 1024; n++)
+	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
 			sum += n;
 		}
-}
-printf("%d\n", sum);
-return (0);
-
+	}
+	printf("%d\n", sum);
+	return (0);
 }
