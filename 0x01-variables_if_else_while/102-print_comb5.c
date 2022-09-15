0fad7e219bf4ea1eb15a310987568a9dfe730966
nebiyat <nebiyats@gmail.com>
2022-09-14 18:21:02 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,44 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints 3 combination of numbers
- *
- * Return: Always (Success)
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
  */
 int main(void)
 {
-	int c, i, k, j;
-
-	for (c = 48; c <= 57; c++)
+	int c;
+	int d;
+	int e = 0;
+	
+	while (e < 10)
 	{
-		for (i = 48; i <= 57; i++)
-			for (k = 48; k <= 57; k++)
-			       for (j =48; j <= 57; j++)
-			       if (((k +j) > (c + i) && k >= c) || c < k)
-			       {
-			putchar(c);
-	 putchar(i);
-putchar(' ');
-putchar(k);
-putchar(j);
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 
-if (c + i + k + j == 227 && c == 57)
-{
-	break;
 }
-else
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-}
-}
-}
-}
-
-putchar('\n');
-
-return (0);
-}
-
