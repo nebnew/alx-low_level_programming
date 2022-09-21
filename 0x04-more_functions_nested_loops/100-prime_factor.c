a636d2ca8aa3b3aeccc396a9328485483a184695
nebiyat <nebiyats@gmail.com>
2022-09-20 23:45:29 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#include "main.h"
+#include <math.h>
 
 /**
- * main -  calculate largest prime of 612852475143
- * Return: Success Always
+ * main - Prints the largest prime factor of the number 612852475143
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-	long int x = 612852475143;
-	long int py;
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
-	for (py = 2; py < x; py++)
+	for (x = 1; x <= square; x++)
 	{
-		if (x % py == 0
-				x = x / py;
-				}
-				printf("%ld\n", py);
-				return (0);
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
 }
