53a52f6328f5e56607e211e4ceef56e5e17a3304
nebiyat <nebiyats@gmail.com>
2022-10-06 15:11:45 -0700

    101-mul.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,6 +5,7 @@
 /**
  * malloc_checked - Allocates memory
  * @b: the size to allocate
+ *
  * Return: Nothing.
  */
 void *malloc_checked(unsigned int b)
@@ -12,7 +13,9 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 	p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
+
 	return (p);
 }
